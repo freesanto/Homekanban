2a5a58795fd489d84104971963ececb4bd27ee89
Jonathan <jonathan@qq.com>
2023-07-30 12:37:33 +0800

    123

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./footer.scss";
+import "./Footer.module.scss";
 
 export default function Footer() {
   const disclaimer = `The cost ranges are  from nationally AI repair cost guides and are designed for budget purposes only. The costs reflect prices in typical metropolitan areas. The User should always consult a licensed qualified contractor for repair options and costs for major items as repair costs can vary widely dependent upon quality of materials used and economic conditions. Obtain two or three estimates from reputable contractors  actual costs may vary.`;
