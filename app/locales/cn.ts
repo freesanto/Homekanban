de594b8aee7646bd53aa637fc9a6584eacf207fb
freesanto <129162347+freesanto@users.noreply.github.com>
2023-08-16 11:15:02 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -1,8 +1,7 @@
 import { SubmitKey } from "../store/config";
-import { LocaleType } from "./index";
 
 // if you are adding a new translation, please use PartialLocaleType instead of LocaleType
-const cn: LocaleType = {
+const cn = {
   WIP: "Coming Soon...",
   Error: {
     Unauthorized:
@@ -337,7 +336,6 @@ const cn: LocaleType = {
     NotShow: "Never Show Again",
     ConfirmNoShow: "Confirm to disable？You can enable it in settings later.",
   },
-
   URLCommand: {
     Code: "检测到链接中已经包含访问码，是否自动填入？",
     Settings: "检测到链接中包含了预制设置，是否自动填入？",
@@ -364,7 +362,6 @@ type DeepPartial<T> = T extends object
       [P in keyof T]?: DeepPartial<T[P]>;
     }
   : T;
-
 export type LocaleType = typeof cn;
 export type PartialLocaleType = DeepPartial<typeof cn>;
 export default cn;
