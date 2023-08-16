c070c3862537d64f330d792e106e9f56a52dae99
freesanto <129162347+freesanto@users.noreply.github.com>
2023-08-16 10:57:13 +0800

    Update index.ts

--- app/locales/index.ts
+++ app/locales/index.ts
@@ -16,8 +16,8 @@ import ar from "./ar";
 import bn from "./bn";
 import { merge } from "../utils/merge";
 
-import type { LocaleType } from "./en";
-export type { LocaleType, PartialLocaleType } from "./en";
+import type { LocaleType } from "./cn";
+export type { LocaleType, PartialLocaleType } from "./cn";
 
 const ALL_LANGS = {
   en,
