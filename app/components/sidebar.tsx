1b764bbb2fedef0a75b41a69cd997aacec4d5bdc
freesanto <129162347+freesanto@users.noreply.github.com>
2023-08-17 23:47:19 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -123,7 +123,7 @@ export function SideBar(props: { className?: string }) {
           Homekanban
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Your AI Cost assessment
+          Your AI Cost Assessment
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           {/*<LogoIcon/>*/}
