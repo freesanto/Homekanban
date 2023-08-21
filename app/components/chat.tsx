d52b2c97619d42beb374bc7ac1bce6339102336b
freesanto <129162347+freesanto@users.noreply.github.com>
2023-08-21 19:03:43 +0800

    Update chat.tsx

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -1368,13 +1368,13 @@ export function Chat() {
                 if (Lang == "English") {
                     luckyCommand = "Based on the information below, please provide a repair quote list with estimated costs for \n" +
                         "materials and labor in " + Region + ", USA  and calculate the total Estimated Cost Range with the numerical, \n" +
-                        "Approximate numbers are fine if exact figures are unavailable. Please provide the output in English  \n" +
-                        ", and please output with form sheet format :           " + userInput
+                        "Approximate numbers are fine if exact figures are unavailable. Please output in English  version \n" +
+                        ", and with the form sheet format output :           " + userInput
                 } else {
                     luckyCommand = "Based on the information below, please provide a repair quote list with estimated costs for \n" +
-                        "materials and labor in " + Region + ", USA  and calculate the total Estimated Cost Range with the numerical, \n" +
-                        "Approximate numbers are fine if exact figures are unavailable. Please provide the quote list in both (English) \n" +
-                        "and in (" + Lang + ")  two versions, and please output with form sheet format:      " + userInput
+                        "materials and labor in " + Region + ", USA  and calculate the total Estimated Cost Range \n" + 
+                        "with the numerical,Approximate numbers are fine if exact figures are unavailable. Please  output in \n" + 
+                        "both (English) version and (" + Lang + ")  version, and  with the form sheet format output:      " + userInput
                 }
                 doSubmit(luckyCommand);
             }}
