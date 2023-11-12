a704d013f7eafe5c17fe42889f5fdf6560c15a2f
freesanto <129162347+freesanto@users.noreply.github.com>
2023-11-12 16:22:58 +0800

    Update prompt.ts

--- app/store/prompt.ts
+++ app/store/prompt.ts
@@ -158,7 +158,8 @@ export const usePromptStore = create<PromptStore>()(
         fetch(PROMPT_URL)
           .then((res) => res.json())
           .then((res) => {
-            let fetchPrompts = [res.en, res.cn];
+            //let fetchPrompts = [res.en,res.cn];
+            let fetchPrompts = [res.en];
             if (getLang() === "cn") {
               fetchPrompts = fetchPrompts.reverse();
             }
@@ -182,7 +183,8 @@ export const usePromptStore = create<PromptStore>()(
             const allPromptsForSearch = builtinPrompts
               .reduce((pre, cur) => pre.concat(cur), [])
               .filter((v) => !!v.title && !!v.content);
-            SearchService.count.builtin = res.en.length + res.cn.length;
+            //SearchService.count.builtin = res.en.length + res.cn.length;
+            SearchService.count.builtin = res.en.length ;
             SearchService.init(allPromptsForSearch, userPrompts);
           });
       },
