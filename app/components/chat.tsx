e99f7da01033ceec19d4dc58346544055e7b76af
freesanto <129162347+freesanto@users.noreply.github.com>
2023-11-27 15:46:41 +0800

    Update chat.tsx

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -1347,13 +1347,13 @@ export function Chat() {
         luckyCommand = "Based on the information below, please provide a repair quote list along with estimated costs for materials and \n" +
           "labor separately based " + myCity + "  City  " + myCountry + " . Please Provide costs estimated numerical range if the accurate estimates \n" +
           "is unavailable, and calculate the total Costs Range with the numerical, Please output all quote list information in  \n" + 
-          " English version with the table format ：\n" + 
+          " English version with tabular format.\n" + 
           "| " + userInput;
       } else {
         luckyCommand = "Based on the information below, please provide a repair quote list along with estimated costs for materials and \n" +
           "labor separately based in " + myCity + "  City， " + myCountry + " . Please Provide costs estimated numerical range if the accurate estimates \n" +
           "is unavailable, and calculate the total Costs Range with the numerical, Please output all quote list information in both \n" + 
-          " English and " + Lang + " versions list separately with the table format ：\n " + 
+          " English and " + Lang + " versions list separately with tabular format.\n " + 
           "| " + userInput;
       }
       doSubmit(luckyCommand);
